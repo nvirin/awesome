6b8d06222e36c22e586a5d5ec24a14c8714010fa
nvirin <9192826+nvirin@users.noreply.github.com>
2019-01-27 21:02:19 +0100

    Update App.js

--- App.js
+++ App.js
@@ -9,28 +9,34 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { WebView } from 'react-native-webview';
 
-const instructions = Platform.select({
+
+/*const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
+*/
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+      <WebView
+        source={{uri: 'https://democuisine.nicolasvirin.com'}}
+        style={{marginTop: 20}}
+      />
+     /* <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+   */ );
   }
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -47,4 +53,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+});*/
